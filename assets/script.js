6116bf6d46881b5a3dcb706e46a3beff2fafca61
Sip <sipg789@gmail.com>
2022-03-08 13:07:06 -0600

    added current day

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// TODO: display current day at top of calendar jquery's UI datepicker (5.4.4)
+var today = document.querySelector("#currentDay");
+    var currentDate = moment();
+today.textContent = currentDate.format('MMMM Do YYYY, h:mm:ss a');
+
+
+// TODO: time blocks for standard business hours (8-5pm) **use jquery grid layouts bootstrap (5.2.4)
+
+// TODO: color code time blocks to indicate past, present, future (audits 5.4.6)
+
+// TODO: when I click on a time block I can enter an event (onClick function 5.2 lesson)
+
+// TODO: save to local storage 
