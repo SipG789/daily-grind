e2fe762cd95d78033cd6e8c741663705a97958fe
Sip <sipg789@gmail.com>
2022-03-13 20:19:39 -0500

    finalized color code

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # daily-grind
 Work Day Scheduler
+
+### In this application I attempted to create a work day scheduler. I was able to work off of the code provided, and added to it. I was able to make the save button work but for some reason everything comes back as underfined, I feel like I almost have the last piece to the puzzle but I could not figure it out.
+
+### I used a lot of module 5 to build my app, I also used taskmaster pro and some guidance from classmates. 
+
+The following image shows a screenshot of the application.
+![screenshot](./assets/deployed-app.png)
+
+## Accepted Criteria 
+
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+THEN I can enter an event
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+The  link to the website is here: https://sipg789.github.io/daily-grind/
--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,65 @@
-var tasks = {};
-
 // TODO: display current day at top of calendar jquery's UI datepicker (5.4.4)
 var today = document.querySelector("#currentDay");
 var currentDate = moment();
 today.textContent = currentDate.format('MMMM Do YYYY, h:mm:ss a');
 
-// TODO: time blocks for standard business hours (6am-6pm)
+//TODO: save edited event blocks 
+$(".saveBtn").on("click", function (event) {
+
+    event.preventDefault();
+    //get current value
+    var text = $(this)
+        .siblings(".list-group-task")
+        .val();
+
+    // get parent's id attribute 
+    var status = $(this)
+        .siblings(".hour")
+        .text();
+
+        localStorage.setItem(status, JSON.stringify(text));
+   
+});
 
 // TODO: save to local storage 
 
-var loadTasks = function () {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-  
-      if (!tasks) {
-        tasks = {
-        toDo: []
-        };
-      }
-    $.each(tasks, function (list, arr) {
-        arr.forEach(function(task) {
-            createTask(task.text, list);
-        });
-    });
-  };
+function loadTasks () {
 
-var saveTasks = function () {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+    $("#6").val(localStorage.getItem("6"));
+    $("#7").val(localStorage.getItem("7AM"));
+    $("#8").val(localStorage.getItem("8AM"));
+    $("#9").val(localStorage.getItem("9AM"));
+    $("#10").val(localStorage.getItem("10AM"));
+    $("#11").val(localStorage.getItem("11AM"));
+    $("#12").val(localStorage.getItem("12PM"));
+    $("#1").val(localStorage.getItem("1PM"));
+    $("#2").val(localStorage.getItem("2PM"));
+    $("#3").val(localStorage.getItem("3PM"));
+    $("#4").val(localStorage.getItem("4PM"));
+    $("#5").val(localStorage.getItem("5PM"));
+    $("#6eve").val(localStorage.getItem("6PM"));
+    
 };
+
+loadTasks();
 // TODO: color code time blocks to indicate past, present, future (audits 5.4.6)
+var displayColors = function (taskEl) {
+    $(taskEl).find("p").text().trim();
+
+     var time = moment().hour();
+
+     $(taskEl).removeClass("list-group-task");
+
+     if (moment().isAfter(time)) {
+         $(taskEl).addClass("future");
+     }
+     if (moment().isBefore(time)) {
+         $(taskEl).addClass("past");
+     } 
+     else if (moment() === currentDate) {
+         $(taskEl).addClass("present");
+     }
+};
 
 // TODO: when I click on a time block I can enter an event (onClick function 5.2 lesson)
 $(".list-group").on("click", "p", function () {
@@ -42,33 +74,12 @@ $(".list-group").on("click", "p", function () {
     // edit task block 
     $(this).replaceWith(textInput);
 
+    // check due date 
+    displayColors(textInput);
+
+    
     textInput.trigger("focus");
 });
 
-//TODO: save edited event blocks 
-$(".list-group").on("blur", "textarea", function () {
-    //get current value
-    var text = $(this)
-        .val()
-        .trim();
 
-    // get parent's id attribute 
-    var status = $(this)
-        .closest(".list-group")
-        .attr("id")
-        .replace("list-", "");
 
-    // get task's position 
-    var index = $(this)
-        .closest(".list-group-item")
-        .index();
-
-        tasks[status][index].text = text;
-        saveTasks();
-
-    var taskP = $("<p>")
-        .addClass("m-1")
-        .text(text);
-
-    $(this).replaceWith(taskP);
-});
